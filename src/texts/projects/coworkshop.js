b7f40a7d663e29e6714b4f99183d94d653969d9e
Fabien <fabien.ogli@etu.univ-lyon1.fr>
2018-12-27 17:29:31 -0500

    remove typo

--- src/texts/projects/coworkshop.js
+++ src/texts/projects/coworkshop.js
@@ -2,7 +2,7 @@ export default {
   id: 1,
   title: "CoWorkshop",
   content: {
-    en: "CoWorkshop is a web solution which allow the user to participate and share his projects.",
+    en: "CoWorkshop is a web solution which allows the user to participate and share his projects.",
     fr: "CoWorkshop est une solution web permettant de participer et de proposer des projets.",
   },
   logos: ['javascript', 'ruby']
